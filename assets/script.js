06cac7755c401cb5c71cd030ce019b287195e60b
hart <100047482+loyallhart@users.noreply.github.com>
2023-03-18 03:31:15 -0400

    start js

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// Add Search Button Event.Listener
+// Store City 
+//api- https://openweathermap.org/forecast5#cityid5
+
+
+const searchForm = document.getElementById("searchForm");
+let searchBtn = document.getElementById("searchBtn");
+
+const apiKey = "a69f566d76c60b3b1c5949c3a3d79975"
